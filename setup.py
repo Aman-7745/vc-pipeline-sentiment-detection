71399e67816b797b9b495af32e52cea5d922dede
Aman <contactfromaman@gmail.com>
2025-07-06 10:40:21 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='integrating existing project with AWS and dvc pipeline',
+    author='Aman',
+    license='',
+)
